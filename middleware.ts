5b7c86ed44cfb4d2d2abfddd9f291e1f0644c38e
bhushansonsale09 <bhushanofficial0908@gmail.com>
2024-07-15 22:56:46 +0530

    implement stripe

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,21 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-const isPublicRoute = createRouteMatcher(['/','/sign-in(.)','/sign-up(.)','/events/:id', '/api/webhook/clerk', '/api/webhook/stripe', '/api/uploadthing(.*)']);
-
-export default clerkMiddleware((auth, req) => {
-    if(!isPublicRoute(req)){
-        auth().protect();
-    }
+import { clerkMiddleware } from "@clerk/nextjs/server";
+ 
+export default clerkMiddleware({
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
 });
-
+ 
 export const config = {
-  matcher: ["/((?!.\\..|_next).)", "/", "/(api|trpc)(.)"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+ 
