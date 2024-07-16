1497c3591486f39511a323555bea06f06bb9b7d4
bhushansonsale09 <bhushanofficial0908@gmail.com>
2024-07-16 23:45:02 +0530

    run

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  '/',
-  '/events/:id',
-  '/api/webhook/clerk',
-  '/api/webhook/stripe',
-  '/api/uploadthing(.*)'
-]);
+const isPublicRoute = createRouteMatcher(['/','/sign-in(.)','/sign-up(.)','/events/:id', '/api/webhook/clerk', '/api/webhook/stripe', '/api/uploadthing(.*)']);
 
 export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) {
-    auth().protect();
-  }
+    if(!isPublicRoute(req)){
+        auth().protect();
+    }
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.\\..|_next).)", "/", "/(api|trpc)(.)"],
 };
